5080328a128208d729e64e5a7053489ae4c06d6f
The_Wizard ඞ <103869920+Th3-Wizard@users.noreply.github.com>
2022-11-08 14:09:17 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN =  os.environ.get('bot_token','5472143789:AAF5Wm1WZqGHnH_Uvmj0iVfWrNtsUadslGc')
-API_ID =  os.environ.get('api_id','18641760')
-API_HASH = os.environ.get('api_hash','b7b026ce9d1d36400c02dc21d8df53a3')
+BOT_TOKEN =  os.environ.get('bot_token','5589571436:AAHKPwzyNIISViNs39oaICkYL_595dBT618')
+API_ID =  os.environ.get('api_id','15453386')
+API_HASH = os.environ.get('api_hash','5589571436:AAHKPwzyNIISViNs39oaICkYL_595dBT618')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','adryes95').split(';')
-#ACCES_USERS = ('tl_admin_user','adryes95','Dr_Stranger0_0')
+ACCES_USERS = os.environ.get('tl_admin_user','EL_Wizard').split(';')
+#ACCES_USERS = ('tl_admin_user','EL_Wizard','Stvz20')
 #ACCES_USERS = os.environ.get(ACCES_USERS)
 PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','socks5h://KIDHKDYDJHJHGIYJJHGGGDYJIDIDRKGILKGGKG'))
 
